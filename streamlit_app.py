0d325f1a48f90ee05214d4b4eb0dbf948da45bbd
Wenxin Xu <39394617+ixuwenxin@users.noreply.github.com>
2025-09-22 07:43:45 +0000

    upload file widget added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pandas as pd
 
 st.title("My first app")
 st.sidebar.checkbox("Select")
+
+# 允许用户上传CSV文件
+uploaded_file = st.file_uploader("上传CSV文件", type=["csv"])
+if uploaded_file is not None:
+	# 读取CSV内容
+	df = pd.read_csv(uploaded_file)
+	st.write("### 文件内容：")
+	st.dataframe(df)
