e4bc352b2d3f29098c04011b26edb29ed70eb266
Wenxin Xu <39394617+ixuwenxin@users.noreply.github.com>
2025-09-22 07:46:23 +0000

    修改成允许上传excel文件

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,10 +4,10 @@ import pandas as pd
 st.title("My first app")
 st.sidebar.checkbox("Select")
 
-# 允许用户上传CSV文件
-uploaded_file = st.file_uploader("上传CSV文件", type=["csv"])
+# 允许用户上传Excel文件
+uploaded_file = st.file_uploader("上传Excel文件", type=["xls", "xlsx"])
 if uploaded_file is not None:
-	# 读取CSV内容
-	df = pd.read_csv(uploaded_file)
-	st.write("### 文件内容：")
-	st.dataframe(df)
+    # 读取Excel内容
+    df = pd.read_excel(uploaded_file)
+    st.write("### 文件内容：")
+    st.dataframe(df)
